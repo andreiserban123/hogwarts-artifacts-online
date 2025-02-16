b9792ecd07316ede3dc8b63e50f2409fa6e99dc4
andreiserban123 <andyspeed2003@gmail.com>
2025-02-16 20:15:31 +0200

    this is the first commit.

---
+++ src/test/java/ro/ase/ro/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.ase.ro.hogwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
